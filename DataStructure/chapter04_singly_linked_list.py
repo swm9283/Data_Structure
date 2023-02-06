6cbb07a7a3c5b75ba0b827b90631c13b038f03d3
uminsong1512 <uminsong1512@gmail.com>
2023-02-06 18:23:19 +0900

    singly linked list 02 constrcutor

--- DataStructure/chapter04_singly_linked_list.py
+++ DataStructure/chapter04_singly_linked_list.py
@@ -1,43 +1,111 @@
-class Node():
-    def __init__(self,data):
+# 단순 연결 리스트 구현
+
+# Node class 생성.
+class Node:
+    def __init__(self, data):
         self.data = data
         self.link = None
 
-node1 = Node("다현")
 
-node2 = Node("정연")
-node1.link = node2
+# print_nodes함수
+def print_nodes(start):
+    current = start
+    if current.link == None:
+        print("현재 노드는 마지막 노드입니다.")
+        return
+    print(current.data, end=" ")
+    while current.link != None:
+        current = current.link
+        print(current.data, end=" ")
+    print()  # 띄어쓰기.
 
-node3 = Node("쯔위")
-node2.link = node3
+#노드 삽입
+def insert_node(find_data,insert_data):
+    global memory,head,current,pre
 
-node4 = Node("사나")
-node3.link = node4
+    if find_data == head.data:
+        node = Node(insert_data)
+        node.link = head
+        head = node
+        return
 
-node5 = Node("지효")
-node4.link = node5
+    current = head
+    while current.link != None:
+        pre = current
+        current = current.link
+        if current.data == find_data:
+            node = Node(insert_data)
+            node.link = current
+            pre.link = node
+            return
+    node = Node(insert_data)
+    node.link = None
+    current.link = node
 
-#node 삽입
-new_node = Node("재남")
-new_node.link = node3
-node2.link = new_node
+#노드 삭제
+def delete_node(delete_data):
+    global memory,head,current,pre
 
-#node 출력.
-current = node1
-print(current.data,end=" ")
-while current.link != None:
-    current = current.link
-    print(current.data,end =" ")
+    if delete_data ==head.data:
+        print("첫 노드가 삭제됨.")
+        current = head
+        head = head.link
+        del(current)
+        return
 
-#node 삭제.
-#화살표를 먼저 수정하고, 노드를 삭제.
-node2.link=node3
-del(new_node)
-current = node1
-print(current.data,end=" ")
-while current.link != None:
-    current = current.link
-    print(current.data,end =" ")
+    current = head
+    while current.link != None:
+        pre = current
+        current = current.link
+        if current.data == delete_data:
+            print("중간 노드가 삭제됨.")
+            pre.link = current.link
+            del(current)
+            return
+    print("아무 노드가 삭제되지 않음.")
 
 
+
+
+
+
+
+
+
+
+
+# 전역 변수 생성
+memory = list()
+head, current, pre = None, None, None
+data_array = [f"{i}" for i in range(5)]
+
+# main 함수 생성
+if __name__ == "__main__":
+    # 첫 번쨰 노드 생성
+    node = Node(data_array[0])
+    head = node
+    memory.append(node)
+
+    # 두 번째 노드 생성
+    for i in data_array[1:]:
+        pre = node
+        node = Node(i)
+        pre.link = node
+        memory.append(node)
+    print(data_array)
+    print_nodes(head)
+
+    insert_node("2","200")
+    print_nodes(head)
+    insert_node("0","1000")
+    print_nodes(head)
+
+    delete_node("1000")
+    print_nodes(head)
+
+    delete_node("4")
+    print_nodes(head)
+
+    delete_node("50")
+    print_nodes(head)
 
